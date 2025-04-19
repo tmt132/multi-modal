613f003a6fcd442fb3c68c805c28863f3bf01e2f
unknown <tmdgml11342@gmail.com>
2025-04-19 10:59:14 +0900

    fix: alias 추가

--- examples/dynamic-modals-example/vite.config.ts
+++ examples/dynamic-modals-example/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "multi-modal": path.resolve(__dirname, "../../dist"), // 실제 소스 폴더로 연결
+    },
+  },
+});
